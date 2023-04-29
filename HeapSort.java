7caee529dba3cacf96bbe3360d7e5213f6cdfced
satishck <113525378+satishck@users.noreply.github.com>
2023-04-29 19:09:25 +0530

    Update HeapSort.java

--- HeapSort.java
+++ HeapSort.java
@@ -23,7 +23,25 @@ static void heapify(int a[], int n, int i)
         heapify(a, n, largest);  
     }  
 }  
-
+    int largest = i; // Initialize largest as root  
+    int left = 2 * i + 1; // left child  
+    int right = 2 * i + 2; // right child  
+    // If left child is larger than root  
+    if (left < n && a[left] > a[largest])  
+        largest = left;  
+    // If right child is larger than root  
+    if (right < n && a[right] > a[largest])  
+        largest = right;  
+    // If root is not largest  
+    if (largest != i) {  
+        // swap a[i] with a[largest]  
+        int temp = a[i];  
+        a[i] = a[largest];  
+        a[largest] = temp;  
+          
+        heapify(a, n, largest);  
+    }  
+}  
 public static void main(String args[])   
 {  
     int a[] = {45, 7, 20, 40, 25, 23, -2};  
